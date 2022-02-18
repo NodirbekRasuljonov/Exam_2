77acf7d9d419b824e68f9e16672898e62c08ddab
NodirbekRasuljonov <nodirbekmaqsudjonovich@gmail.com>
2022-02-18 10:43:23 +0500

    assets and flutter_svg added

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,4 +2,14 @@ import 'package:flutter/material.dart';
 
 class ColorConst {
   static Color kprimary = const Color(0xffFF8843);
+  static Color textcolor = Color(0xffFFFFFF);
+}
+
+class FontConst {
+  static double extrasmall = 8.0;
+  static double small = 12.0;
+  static double medium = 16.0;
+  static double large = 20.0;
+  static double extralarge = 24.0;
+
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,17 +1,51 @@
+import 'dart:async';
+
 import 'package:examtwo/core/components/sizeconfig.dart';
+import 'package:examtwo/core/constants/constants.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {});
+  }
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      height: getH(MediaQuery.of(context).size.height),
-      width: getW(MediaQuery.of(context).size.width),
-      color: Colors.red,
-     )
-    );
+      alignment: Alignment.center,
+      height: MediaQuery.of(context).size.height,
+      width: double.infinity,
+      color: ColorConst.kprimary,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 25,
+              width: 86,
+              child: Text(
+                'SmartFarm',
+                style: TextStyle(
+                    color: ColorConst.textcolor,
+                    fontSize: FontConst.medium,
+                    fontWeight: FontWeight.w400),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
