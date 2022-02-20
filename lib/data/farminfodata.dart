97803a06a6b979eeb7f60d3ff0c8dec962cf7643
NodirbekRasuljonov <nodirbekmaqsudjonovich@gmail.com>
2022-02-20 23:53:48 +0500

    oxirgi oage ham bitdi

--- lib/data/farminfodata.dart
+++ lib/data/farminfodata.dart
@@ -49,3 +49,15 @@ class SigirList {
         'https://images.unsplash.com/photo-1545468800-85cc9bc6ecf7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80'),
   ];
 }
+class Tovuqlist {
+  static List<Sigir> tovuqlist = [
+    Sigir('Tovuq', '1 yoshda', '5000000',
+        'https://images.unsplash.com/photo-1563409236340-c174b51cbb81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80'),
+    Sigir("Shvet Tovug'i", '6 oylik', '4000000',
+        'https://images.unsplash.com/photo-1569396327972-6231a5b05ea8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80'),
+    Sigir('Tovuq', '1 oylik', '5000000',
+        'https://images.unsplash.com/photo-1548550023-2bdb3c5beed7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+    Sigir('Tovuq', '6 oylik', '4000000',
+        'https://images.unsplash.com/photo-1554740760-5db7aca3ec66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=785&q=80'),
+  ];
+}
--- lib/screens/farminfo.dart
+++ lib/screens/farminfo.dart
@@ -157,8 +157,9 @@ class _FarmIfoPageState extends State<FarmIfoPage> {
           SliverToBoxAdapter(
             child: Container(
               width: 350.0,
-              height: 242 * SigirList.sigirlist.length.toDouble(),
+              height: 242 * (SigirList.sigirlist.length / 2).toDouble(),
               child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
                   itemCount: SigirList.sigirlist.length,
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisSpacing: 5.0,
@@ -221,19 +222,117 @@ class _FarmIfoPageState extends State<FarmIfoPage> {
                               right: 5.0,
                               bottom: 5.0,
                               child: Container(
+                                alignment: Alignment.center,
+                                child: Text(SigirList.sigirlist[index].price),
                                 height: 40.0,
                                 width: 145.0,
                                 decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(
                                         RadiusConst.extrasmall),
-                                    color: Color(0xffEFEFEF)),
-                              ))
+                                    color: const Color(0xffEFEFEF)),
+                              )),
                         ],
                       ),
                     );
                   }),
             ),
-          )
+          ),
+          SliverPadding(
+            padding: EdgeInsets.only(left: 20.0, top: 25.0),
+            sliver: SliverToBoxAdapter(
+                child: Container(
+              height: 25.0,
+              width: 89.0,
+              child: Text(
+                'Mahsulotlar',
+                style:
+                    TextStyle(color: Colors.black, fontSize: FontConst.large),
+              ),
+            )),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: 350.0,
+              height: 242 * (SigirList.sigirlist.length / 2).toDouble(),
+              child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: Tovuqlist.tovuqlist.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: 5.0,
+                      mainAxisSpacing: 20.0,
+                      crossAxisCount: 2,
+                      mainAxisExtent: 242.0),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 0.0),
+                      decoration: BoxDecoration(
+                          color: ColorConst.textcolor,
+                          borderRadius:
+                              BorderRadius.circular(RadiusConst.extrasmall)),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                              child: Container(
+                            height: 120.0,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topLeft:
+                                        Radius.circular(RadiusConst.extrasmall),
+                                    topRight: Radius.circular(
+                                        RadiusConst.extrasmall)),
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(
+                                        Tovuqlist.tovuqlist[index].pic))),
+                          )),
+                          Positioned(
+                              top: 140.0,
+                              left: 20.0,
+                              right: 20.0,
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: 20.0,
+                                width: 79.0,
+                                child: Text(
+                                  Tovuqlist.tovuqlist[index].name,
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              )),
+                          Positioned(
+                              top: 165.0,
+                              left: 48.0,
+                              right: 48.0,
+                              child: Container(
+                                height: 17.0,
+                                width: 59.0,
+                                alignment: Alignment.center,
+                                child: Text(
+                                  Tovuqlist.tovuqlist[index].age,
+                                  style:
+                                      TextStyle(color: ColorConst.borderColor),
+                                ),
+                              )),
+                          Positioned(
+                              left: 5.0,
+                              right: 5.0,
+                              bottom: 5.0,
+                              child: Container(
+                                alignment: Alignment.center,
+                                child: Text(Tovuqlist.tovuqlist[index].price),
+                                height: 40.0,
+                                width: 145.0,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(
+                                        RadiusConst.extrasmall),
+                                    color: const Color(0xffEFEFEF)),
+                              )),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+          ),
         ],
       ),
     );
